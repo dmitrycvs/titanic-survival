a000a1d7b33125a4becf381f9a499b65ea58c72e
scott1q <cvasiuc.dima@gmail.com>
2024-03-28 23:55:40 +0200

    Data was processed and cleaned

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import os
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
 
 file_path = os.path.join(os.path.dirname(__file__), 'sample-data/Titanic-Dataset.csv')
+df = pd.read_csv(file_path)
+columns = df.columns.tolist()
 
-data = pd.read_csv(file_path)
-print(data.head())
+# Deleting Missing Values
+df.dropna(inplace=True)
+
+# Data Cleaning
+df.drop_duplicates(inplace=True)
+
+for col in columns:
+    if df[col].dtype == 'object':
+        df[col] = df[col].str.lower()
+
+# Data splitting
+y = df['Survived']
+X = df.drop(columns=['Survived'])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
